0fbe0366b77e93b3c2bcb051d3dd90b0f54f077e
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-02-10 14:59:24 +0200

    feat: add react-router, redirection logic and check login status logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import { useContext } from 'react'
+import { Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { MainContext } from './context/MainContext'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
+  const { isLoggedIn } = useContext(MainContext)
   return (
     <>
-		Hello App
-		</>
-  );
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/profile'>
+            {isLoggedIn ? <ProfilePage /> : <Redirect to='/' />}
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
